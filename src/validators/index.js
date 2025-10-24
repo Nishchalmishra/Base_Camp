62627a50c7d885639065f79a3f1a4159f0e10a48
Nishchalmishra <nishchalmishra14@gmail.com>
2025-10-24 15:25:40 +0530

    update middlewares and validators

---
+++ src/validators/index.js
@@ -0,0 +1,33 @@
+import { body } from "express-validator"
+
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is required")
+            .isLowercase()
+            .withMessage("Username must be lowercase")
+            .isLength({ min: 3, max: 20 })
+            .withMessage("Username must be between 3 and 20 characters"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required")
+            .isLength({ min: 8 })
+            .withMessage("Password must be at least 8 characters long")
+            
+    ]
+}
+
+
+export {
+    userRegisterValidator
+}
