da7d91c11e74a3f4405aab17cdaf5626d1a4ec07
Nishchal Mishra <nishchalmishra14@gmail.com>
2026-01-09 11:53:55 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,19 +9,19 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // unsecured route
-router.route("/register").post(userRegisterValidator(), validate,registerUser)
-router.route("/login").post(userLoginValidator(),validate, login)
+router.route("/register").post(userRegisterValidator(), registerUser)
+router.route("/login").post(userLoginValidator(), login)
 router.route("/verify-email/:verificationToken").get( verifyEmail)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/forgot-password").post(userForgotPasswordValidator(), validate, forgotPassword)
-router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(), validate, resetForgotPassword)
+router.route("/forgot-password").post(userForgotPasswordValidator(),  forgotPassword)
+router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(),  resetForgotPassword)
 
 
 // secured route
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/get-current-user").post(verifyJWT, getCurrentUser)
 router.route("/resend-email-verification").post(verifyJWT, resendEmailVerification)
-router.route("/change-password").post(verifyJWT, userForgotPasswordValidator(), validate, changeCurrentPassword)
+router.route("/change-password").post(verifyJWT, userForgotPasswordValidator(), changeCurrentPassword)
 
 
 
